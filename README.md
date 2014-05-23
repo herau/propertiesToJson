7c5844d846ff964408a093626c726acedf1b55a6
n27 <n27@3ds.com>
2014-05-23 17:53:11 +0200

    missing file for test unit update readme

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ propertiesToJson
 ================
 
 conversion of properties files to JSON files
+
+mvn clean install
+
+java -jar propertiesToJson.jar /your/path/
---
+++ src/test/java/FilesManager.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/**
+ * Created by herau on 23/05/14.
+ */
+public class FilesManager {
+
+    /**
+     * recursive delete
+     * @param path path of a directory or a file
+     * @throws java.io.IOException
+     */
+    public static void delete(Path path) throws IOException {
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.delete(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.delete(dir);
+                return FileVisitResult.CONTINUE;
+            }
+
+        });
+    }
+}
