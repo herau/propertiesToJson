01d5929c2e125e260c038b8e49f47cdbe1deecbf
n27 <n27@3ds.com>
2014-05-23 17:10:39 +0200

    Unit Test : only one file conversion

--- src/main/java/PropertiesToJsonFileVisitor.java
+++ src/main/java/PropertiesToJsonFileVisitor.java
@@ -1,4 +1,8 @@
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonGenerator;
+
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,9 +11,6 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Map;
 import java.util.Properties;
 
-/**
- * Created by n27 on 23/05/14.
- */
 public class PropertiesToJsonFileVisitor extends SimpleFileVisitor<Path> {
 
     @Override
@@ -17,11 +18,20 @@ public class PropertiesToJsonFileVisitor extends SimpleFileVisitor<Path> {
         // load properties file
         Properties properties = new Properties();
         properties.load(Files.newInputStream(file));
+        // create new file
+        Path newFile = file.getParent().resolve(file.getFileName().toString().split("\\.")[0]);
+        OutputStream newFileOutputStream = Files.newOutputStream(newFile);
         // iterate and convert to JSON
+        JsonFactory f = new JsonFactory();
+        JsonGenerator g = f.createJsonGenerator(newFileOutputStream);
+
+        g.writeStartObject();
         for (Map.Entry<Object, Object> property : properties.entrySet()) {
-            System.out.println();
+            g.writeStringField((String) property.getKey(), (String) property.getValue());
         }
-        
+        g.writeEndObject();
+        g.close();
+
         return FileVisitResult.CONTINUE;
     }
 }
--- src/test/java/BootstrapTest.java
+++ src/test/java/BootstrapTest.java
@@ -1,9 +1,23 @@
 import org.junit.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Properties;
+
 public class BootstrapTest {
 
     @Test
     public void testConversion() throws Exception {
+        Path tempFile = Files.createTempFile("test", ".properties");
 
+        Properties properties = new Properties();
+        properties.setProperty("key", "value");
+
+        properties.store(Files.newOutputStream(tempFile, StandardOpenOption.CREATE), "no comments");
+
+        Files.walkFileTree(tempFile, new PropertiesToJsonFileVisitor());
+
+        FilesManager.delete(tempFile);
     }
 }
